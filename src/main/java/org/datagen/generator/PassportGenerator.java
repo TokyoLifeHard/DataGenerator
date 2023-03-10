de07cba1e8dfc97e6734e242ad7d9c24622e4178
Kawaiiskii <antonzhadnov@gmail.com>
2023-03-10 16:39:44 +0300

    add generate Okato medtod and whom issued metod

--- src/main/java/org/datagen/generator/PassportGenerator.java
+++ src/main/java/org/datagen/generator/PassportGenerator.java
@@ -10,60 +10,69 @@ import java.util.Random;
 
 public class PassportGenerator {
     private static final Reader READER = new Reader();
-    private static final String FEMALE_LAST_NAME = "";
-    private static final String FEMALE_NAME = "";
-    private static final String FEMALE_PATRONYMIC = "";
-    private static final String MALE_LAST_NAME = "";
-    private static final String MALE_NAME = "";
-    private static final String MALE_PATRONYMIC = "";
+    private static final String FEMALE_LAST_NAME = "src/main/resources/femaleLastName.txt";
+    private static final String FEMALE_NAME = "src/main/resources/femaleNames.txt";
+    private static final String FEMALE_PATRONYMIC = "src/main/resources/patronymicFemale.txt";
+    private static final String MALE_LAST_NAME = "src/main/resources/malelastName.txt";
+    private static final String MALE_NAME = "src/main/resources/maleNames.txt";
+    private static final String MALE_PATRONYMIC = "src/main/resources/patronymicMale.txt";
+    private static final String OKATO = "src/main/resources/Okato.txt";
 
-    public Passport generate(){
+
+    public Passport generate() throws IOException {
         char sex = generateSex();
-
-
-        return null;
+        if (sex == 'm'){
+           return generateMale();
+        }else {
+            return genearteFemale();
+        }
     }
 
     private Passport genearteFemale() throws IOException {
         String birthDate = generateBirthDate();
-        String dateIssued = caluclateDateIssued(birthDate);
         return new Passport.Builder()
                 .firstName(READER.getRandomFio(Path.of(FEMALE_NAME)))
                 .secoundName(READER.getRandomFio(Path.of(FEMALE_LAST_NAME)))
                 .patronymic(READER.getRandomFio(Path.of(FEMALE_PATRONYMIC)))
                 .birthDate(birthDate)
+                .whomIssued(whomIssued())
                 .dateIssued(caluclateDateIssued(birthDate))
-                .placeLiving("")
-                .seria("")
-                .number("")
+                .placeLiving("Москва")
+                .seria(generateSeria(birthDate,genearteOkatoNum()))
+                .number(generateNumber())
                 .build();
 
     }
 
     private Passport generateMale() throws IOException {
         String birthDate = generateBirthDate();
-        String dateIssued = caluclateDateIssued(birthDate);
         return new Passport.Builder()
                 .firstName(READER.getRandomFio(Path.of(MALE_NAME)))
                 .secoundName(READER.getRandomFio(Path.of(MALE_LAST_NAME)))
                 .patronymic(READER.getRandomFio(Path.of(MALE_PATRONYMIC)))
                 .birthDate(birthDate)
                 .dateIssued(caluclateDateIssued(birthDate))
-                .placeLiving("")
-                .seria("")
-                .number("")
+                .placeLiving("Москва")
+                .seria(generateSeria(birthDate,genearteOkatoNum()))
+                .number(generateNumber())
                 .build();
 
     }
 
     private String caluclateDateIssued(String birthDate){
-        LocalDate dateBirthdate = LocalDate.parse(birthDate);
 
         StringBuilder dateIssued = new StringBuilder();
-        int dayOfTheMonth = Integer.parseInt(birthDate.split("-")[0]);
+
+        int dayOfTheMonth = Integer.parseInt(birthDate.split("-")[2]);
         int monthNumber = Integer.parseInt(birthDate.split("-")[1]);
 
-        RandomUtils.radmonInts(1,2);
+        if(monthNumber == 2){
+
+        }
+
+        dayOfTheMonth += RandomUtils.radmonInts(1,5);
+        monthNumber += RandomUtils.radmonInts(1,3);
+
         if (isUpper14andYounger20(birthDate)){
            dateIssued.append(caclulateYearIssued(birthDate,14)) ;
         }else if (isUpper20(birthDate) && !isUpper45(birthDate)){
@@ -71,12 +80,13 @@ public class PassportGenerator {
         } else if (isUpper45(birthDate)) {
             dateIssued.append(caclulateYearIssued(birthDate,45));
         }
-        return dateIssued.toString();
+        return dateIssued.append("-").append(monthNumber).append("-").append(dayOfTheMonth).toString();
     }
     private int caclulateYearIssued(String birthDate,int ageIssued){
-        String birthYear = birthDate.split("-")[2];
-        int years = LocalDate.now().getYear() - Integer.parseInt(birthDate);
-        return LocalDate.now().getYear() - (years - ageIssued);
+        String birthYear = birthDate.split("-")[0];
+        System.out.println(birthYear);
+        int years = LocalDate.now().getYear() - Integer.parseInt(birthYear);
+        return LocalDate.now().getYear() - Math.abs(years - ageIssued);
     }
     public String generateBirthDate(){
         Random random = new Random();
@@ -88,7 +98,9 @@ public class PassportGenerator {
 
     private String generateSeria(String birthDate, String okatoData){
         String yearBirth = birthDate.split("-")[0];
-        int last2DigestOfYearBirth = Integer.parseInt(yearBirth.substring(3,4));
+        System.out.println(yearBirth);
+        int last2DigestOfYearBirth = Integer.parseInt(yearBirth.substring(2));
+        System.out.println(last2DigestOfYearBirth);
         String okatoNum = okatoData.split(",")[0];
         return new StringBuilder()
                 .append(okatoNum)
@@ -96,6 +108,22 @@ public class PassportGenerator {
                 .toString();
     }
 
+    private String generateNumber(){
+        StringBuilder passportNumber = new StringBuilder();
+        for (int i = 0; i<6;i++){
+            passportNumber.append(RandomUtils.radmonInts(0,9));
+        }
+        return passportNumber.toString();
+    }
+    private String genearteOkatoNum() throws IOException {
+        return READER.getRandomLine(Path.of(OKATO))[0];
+    }
+
+    private String whomIssued() throws IOException{
+        StringBuilder whomIssued = new StringBuilder();
+        String[] okato = READER.getRandomLine(Path.of(OKATO));
+        return  whomIssued.append("УФМС России по гор ").append(okato[2]).append(" ").append(okato[1]).toString();
+    }
     private boolean isUpper20(String birthDate){
 
         boolean isFullYearsMore20 = LocalDate.now().getYear() - Integer.parseInt(birthDate.split("-")[2]) > 20;
@@ -135,8 +163,10 @@ public class PassportGenerator {
         }
     }
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws IOException {
+        PassportGenerator passportGenerator = new PassportGenerator();
+        Passport passport = passportGenerator.generate();
+        System.out.println(passport.toString());
 
     }
 }
